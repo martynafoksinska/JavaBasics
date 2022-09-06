8411fcdc40f84863ecd255a7bd75c785f83320f6
martynafoksinska <martyna.a.foksinska@gmail.com>
2022-09-06 19:49:36 +0200

    Simple exercises

---
+++ src/test/java/Main14.java
@@ -0,0 +1,12 @@
+public class Main14 {
+
+    public static void main(String[] args) {
+        int[] numbers = {4, 643, 112, 9999, 69};
+        int sum = 0;
+        for (int i = 0; i <= 4 ; i++) {
+            sum += numbers[i];
+        }
+        System.out.println(sum);
+    }
+
+}
---
+++ src/test/java/Main15.java
@@ -0,0 +1,14 @@
+public class Main15 {
+
+    public static void main(String[] args) {
+        int[] numbers = {4, 643, 112, 9999, 69};
+        int sumOdd = 0;
+        for (int i = 0; i <= 4; i++)
+            if (numbers[i] % 2 == 0) {
+                System.out.println(numbers[i]);
+            } else {
+                sumOdd += numbers[i];
+            }
+        System.out.println("SUMA: " + sumOdd + ".");
+    }
+}
