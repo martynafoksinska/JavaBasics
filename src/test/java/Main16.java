6e7eb71d4dd78aa28b48f8f065381730976dd045
Martyna Foksinska <martyna.a.foksinska@gmail.com>
2022-09-07 09:34:32 +0200

    Exercises added

--- src/test/java/Main16.java
+++ src/test/java/Main16.java
@@ -1,2 +1,13 @@
 public class Main16 {
+
+    public static void main(String[] args) {
+        int[] numbers = {4, 643, 112, 9999, -69};
+        int min = 0;
+        for (int i = 0; i <= 4; i++) {
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+        }
+        System.out.print("MIN:" + min);
+    }
 }
---
+++ src/test/java/Main17.java
@@ -0,0 +1,13 @@
+public class Main17 {
+
+    public static void main(String[] args) {
+        int[] numbers = {4, 643, 112, 9999, -69};
+        for (int i = 0; i < 1; i++) {
+            System.out.println(numbers[4]);
+            System.out.println(numbers[3]);
+            System.out.println(numbers[2]);
+            System.out.println(numbers[1]);
+            System.out.println(numbers[0]);
+        }
+    }
+}
